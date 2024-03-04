31c6ebc7817a0f41d08d4c66707865892a7e3477
Kavya J <kavyadevanga15@gmail.com>
2024-03-04 23:18:17 +0530

    first commit - quiz game

---
+++ rock_paper_scissor.py
@@ -0,0 +1,38 @@
+import random
+
+user_win = 0
+computer_win = 0
+
+options = ['rock', 'paper', 'scirrors']
+
+while True:
+    user_input = input("Type Rock/Paper/Scirrors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+
+    computer_pick = options[random_number]
+    print("Computer picked: ", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scirrors":
+        print("you won!")
+        user_win += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("you won!")
+        user_win += 1
+    elif user_input == "scirrors" and computer_pick == "paper":
+        print("you won!")
+        user_win += 1
+    else:
+        print("You lost!")
+        computer_win += 1
+
+print("You won", user_win, "times.")
+print("The computer won", computer_win, "times.")
+print("Goodbye!")
+
+
