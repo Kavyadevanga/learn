b9df994e5660cd8b0efac1d94df70532b5b4c78c
Kavya J <kavyadevanga15@gmail.com>
2024-03-04 21:06:26 +0530

    first commit - quiz game

--- demo.py
+++ demo.py
@@ -1,2 +1,31 @@
-print("Hello")
-print("hi")
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+score = 0
+
+answer = input("CPU stands for: ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("GPU stands for: ")
+if answer.lower() == "Graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("RAM stands for: ")
+if answer.lower() == "Random Access Memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 3) * 100) + "%")
